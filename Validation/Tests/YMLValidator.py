ab35f891e28c73d7ceba7423659e70f2c205e215
joelbarmettlerUZH <joel.barmettler@uzh.ch>
2018-09-27 23:33:45 +0200

    Extended validation tests

--- Validation/Tests/YMLValidator.py
+++ Validation/Tests/YMLValidator.py
@@ -5,6 +5,9 @@ from Validation.YMLValidator import YMLValidator
 class TestValueValidator(unittest.TestCase):
 
     def test_validate_no_unnecessary_files(self):
+        """
+        Test for no unecessary files
+        """
         v = YMLValidator("test-testing-coin")
         self.assertRaises(ValueError, v.validate_no_unnecessary_files)
 
@@ -20,14 +23,23 @@ class TestValueValidator(unittest.TestCase):
         ))
 
     def test_validate_structure(self):
+        """
+        Test for minimal substructure
+        """
         v = YMLValidator("test-testing-coin")
         self.assertTrue(v.validate_structure())
 
     def test_validate_fields(self):
+        """
+        Test validity of all fields
+        """
         v = YMLValidator("test-testing-coin")
         self.assertTrue(v.validate_fields())
 
     def test_end_2_end(self):
+        """
+        End-2-End test, supposed to fail since we have unecessary files
+        """
         v = YMLValidator("test-testing-coin")
         self.assertRaises(ValueError, v.validate)
 
