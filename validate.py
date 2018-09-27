a0f625cae6d1ae6b01229741b295c8fdaf88a5e3
joelbarmettlerUZH <joel.barmettler@uzh.ch>
2018-09-27 23:30:29 +0200

    Validators for unecessary files and minimal substructure

--- validate.py
+++ validate.py
@@ -6,11 +6,14 @@ COINS_DIR = "coins"
 
 
 def get_coins():
-    return os.listdir(os.path.normpath(os.path.join(os.path.dirname(__file__), COINS_DIR)))
+    coins = os.listdir(os.path.normpath(os.path.join(os.path.dirname(__file__), COINS_DIR)))
+    coins.remove("test-testing-coin")
+    return coins
 
 
 if __name__ == "__main__":
     coins = get_coins()
+    print(coins)
     for coin in coins:
         validator = YMLValidator(coin)
         validator.validate()
