eee9451fa988319fc34ec76e820aa45d37d29fee
joelbarmettlerUZH <joel.barmettler@uzh.ch>
2020-09-26 23:30:29 +0200

    Validators for unecessary files and minimal substructure

--- Validation/Tests/YMLValidator.py
+++ Validation/Tests/YMLValidator.py
@@ -4,9 +4,32 @@ from Validation.YMLValidator import YMLValidator
 
 class TestValueValidator(unittest.TestCase):
 
+    def test_validate_no_unnecessary_files(self):
+        v = YMLValidator("test-testing-coin")
+        self.assertRaises(ValueError, v.validate_no_unnecessary_files)
+
+        test_images = [
+            "invalid@128x128.png",
+            "invalid@128x128.jpg"
+        ]
+        test_files = []
+        v = YMLValidator("test-testing-coin")
+        self.assertTrue(v.validate_no_unnecessary_files(
+            test_images=test_images,
+            test_files=test_files
+        ))
+
+    def test_validate_structure(self):
+        v = YMLValidator("test-testing-coin")
+        self.assertTrue(v.validate_structure())
+
+    def test_validate_fields(self):
+        v = YMLValidator("test-testing-coin")
+        self.assertTrue(v.validate_fields())
+
     def test_end_2_end(self):
         v = YMLValidator("test-testing-coin")
-        self.assertTrue(v.validate())
+        self.assertRaises(ValueError, v.validate)
 
 
 if __name__ == "__main__":
--- Validation/YMLValidator.py
+++ Validation/YMLValidator.py
@@ -1,4 +1,5 @@
 from datetime import date
+import os
 
 from Validation.Files import validation_yml, open_yml
 from Validation.ValueValidator import ValueValidator
@@ -13,22 +14,63 @@ class YMLValidator:
         self.__coin_id = coin_id
 
     def validate(self):
-        self.__validate_structure()
-        self.__validate_fields()
-        self.__validate_no_unnecessary_files()
+        self.validate_structure()
+        self.validate_fields()
+        self.validate_no_unnecessary_files()
         return True
 
-    def __validate_fields(self):
+    def validate_fields(self):
         context = {"coin_id": self.__coin_id}
         path = f"[{self.__coin_id}/info.yml]"
         YMLValidator.__recursive_validation(self.__yml_to_validate, self.__validation_yml, context, path=path)
         return True
 
-    def __validate_structure(self):
-        pass
+    def validate_structure(self):
+        path = f"[{self.__coin_id}/info.yml]"
+        YMLValidator.__recursive_structure(self.__yml_to_validate, self.__validation_yml, path)
+        return True
 
-    def __validate_no_unnecessary_files(self):
-        pass
+    def validate_no_unnecessary_files(self, test_images=None, test_files=None):
+        ignore_images = test_images if test_images else []
+        ignore_files = test_files if test_files else []
+
+        detected_images = os.listdir(os.path.normpath(os.path.join(os.path.dirname(__file__), f"./../coins/{self.__coin_id}/images")))
+        detected_files = os.listdir(os.path.normpath(os.path.join(os.path.dirname(__file__), f"./../coins/{self.__coin_id}/files")))
+
+        resolved_images = ignore_images + [self.__yml_to_validate["logo"]] + \
+                         [self.__yml_to_validate["team"]["leader"]["imagename"]] + \
+                         [member["imagename"] for member in self.__yml_to_validate["team"]["members"]]
+        resolved_files = ignore_files + [self.__yml_to_validate["whitepaper"]["file"]]
+
+        for image in detected_images:
+            if image not in resolved_images:
+                raise ValueError(f"An unecessary image was detected: {self.__coin_id}/images/{image}.")
+        for file in detected_files:
+            if file not in resolved_files:
+                raise ValueError(f"An unecessary file was detected: {self.__coin_id}/images/{file}.")
+        return True
+
+    @staticmethod
+    def __recursive_structure(value, validation, path):
+        if validation is None:
+            raise ValueError(f"Illegal Value found in YML: {path}")
+        if type(value) == dict:
+            if type(validation) != dict:
+                raise ValueError(f"Illegal substructure found in YML: {path}")
+            for k, v in value.items():
+                new_path = f"{path}.{k}"
+                if k not in validation.keys():
+                    raise ValueError(f"Unecessary key in YML: {new_path}")
+                validation_value = validation[k]
+                YMLValidator.__recursive_structure(v, validation_value, new_path)
+            return
+        if type(value) == list and type(validation) == dict:
+            for (index, item) in enumerate(value):
+                new_path = f"{path}[{index}]"
+                YMLValidator.__recursive_structure(item, validation, new_path)
+            return
+        if type(value) == list and not validation.startswith("foreach"):
+            raise ValueError(f"Illegal List found in YML: {path}")
 
     @staticmethod
     def __recursive_validation(value, validation, context, path):
@@ -37,8 +79,10 @@ class YMLValidator:
         if type(value) == dict and type(validation) == dict:
             for validation_key, validation_value in validation.items():
 
+                new_path = f"{path}.{validation_key}"
+
                 if validation_key not in value.keys():
-                    raise ValueError(f"A key is missing in your YML: {validation_key}")
+                    raise ValueError(f"A key is missing in your YML: {new_path}")
 
                 values_value = value.get(validation_key)
 
@@ -48,7 +92,6 @@ class YMLValidator:
                 if validation_key == "is_contra":
                     context["opposite"] = value["is_pro"]
 
-                new_path = f"{path}.{validation_key}"
                 YMLValidator.__recursive_validation(values_value, validation_value, context, path=new_path)
             return
 
@@ -71,5 +114,5 @@ class YMLValidator:
 
 if __name__ == "__main__":
     v = YMLValidator("test-testing-coin")
-    v.validate()
+    v.validate_no_unnecessary_files()
 
