96b4f8d63c8bf480d6a562cf16498c666073b262
tryotrix <dan93@gmx.ch>
2020-10-06 22:26:10 +0200

    [joelbarmettlerUZH] Aggretage errors in validation script

--- Validation/YMLValidator.py
+++ Validation/YMLValidator.py
@@ -12,22 +12,38 @@ class YMLValidator:
     def __init__(self, coin_id):
         self.__yml_to_validate = open_yml(f"./coins/{coin_id}/info.yml")
         self.__coin_id = coin_id
+        self.structure_errors = []
+        self.field_errors = []
+        self.file_errors = []
 
     def validate(self):
         self.validate_structure()
         self.validate_fields()
         self.validate_no_unnecessary_files()
-        return True
+        if len(self.structure_errors + self.field_errors + self.file_errors) == 0:
+            return True
+        error_str = ""
+        error_types = {
+            "Structure Errors": self.structure_errors,
+            "Field Errors": self.field_errors,
+            "File Errors": self.file_errors,
+        }
+        for error_name, error_list in error_types.items():
+            if len(error_list) > 0:
+                error_str += f"*{error_name}*\n"
+                for error in error_list:
+                    error_str += f"- {error}\n"
+        raise ValueError(error_str)
 
     def validate_fields(self):
         context = {"coin_id": self.__coin_id}
         path = f"[{self.__coin_id}/info.yml]"
-        YMLValidator.__recursive_validation(self.__yml_to_validate, self.__validation_yml, context, path=path)
+        self.__recursive_validation(self.__yml_to_validate, self.__validation_yml, context, path=path)
         return True
 
     def validate_structure(self):
         path = f"[{self.__coin_id}/info.yml]"
-        YMLValidator.__recursive_structure(self.__yml_to_validate, self.__validation_yml, path)
+        self.__recursive_structure(self.__yml_to_validate, self.__validation_yml, path)
         return True
 
     def validate_no_unnecessary_files(self, test_images=None, test_files=None):
@@ -44,36 +60,34 @@ class YMLValidator:
 
         for image in detected_images:
             if image not in resolved_images:
-                raise ValueError(f"An unecessary image was detected: {self.__coin_id}/images/{image}.")
+                self.file_errors.append(f"An unecessary image was detected: {self.__coin_id}/images/{image}.")
         for file in detected_files:
             if file not in resolved_files:
-                raise ValueError(f"An unecessary file was detected: {self.__coin_id}/images/{file}.")
+                self.file_errors.append(f"An unecessary file was detected: {self.__coin_id}/images/{file}.")
         return True
 
-    @staticmethod
-    def __recursive_structure(value, validation, path):
+    def __recursive_structure(self, value, validation, path):
         if validation is None:
-            raise ValueError(f"Illegal Value found in YML: {path}")
+            self.structure_errors.append(f"Illegal Value found in YML: {path}")
         if type(value) == dict:
             if type(validation) != dict:
-                raise ValueError(f"Illegal substructure found in YML: {path}")
+                self.structure_errors.append(f"Illegal substructure found in YML: {path}")
             for k, v in value.items():
                 new_path = f"{path}.{k}"
                 if k not in validation.keys():
-                    raise ValueError(f"Unecessary key in YML: {new_path}")
+                    self.structure_errors.append(f"Unecessary key in YML: {new_path}")
                 validation_value = validation[k]
-                YMLValidator.__recursive_structure(v, validation_value, new_path)
+                self.__recursive_structure(v, validation_value, new_path)
             return
         if type(value) == list and type(validation) == dict:
             for (index, item) in enumerate(value):
                 new_path = f"{path}[{index}]"
-                YMLValidator.__recursive_structure(item, validation, new_path)
+                self.__recursive_structure(item, validation, new_path)
             return
         if type(value) == list and not validation.startswith("foreach"):
-            raise ValueError(f"Illegal List found in YML: {path}")
+            self.structure_errors.append(f"Illegal List found in YML: {path}")
 
-    @staticmethod
-    def __recursive_validation(value, validation, context, path):
+    def __recursive_validation(self, value, validation, context, path):
 
         # Substructure case
         if type(value) == dict and type(validation) == dict:
@@ -82,7 +96,7 @@ class YMLValidator:
                 new_path = f"{path}.{validation_key}"
 
                 if validation_key not in value.keys():
-                    raise ValueError(f"A key is missing in your YML: {new_path}")
+                    self.field_errors.append(f"A key is missing in your YML: {new_path}")
 
                 values_value = value.get(validation_key)
 
@@ -92,13 +106,13 @@ class YMLValidator:
                 if validation_key == "is_contra":
                     context["opposite"] = value["is_pro"]
 
-                YMLValidator.__recursive_validation(values_value, validation_value, context, path=new_path)
+                self.__recursive_validation(values_value, validation_value, context, path=new_path)
             return
 
         if type(value) == list and type(validation) == dict:
             for (index, item) in enumerate(value):
                 new_path = f"{path}[{index}]"
-                YMLValidator.__recursive_validation(item, validation, context, new_path)
+                self.__recursive_validation(item, validation, context, new_path)
             return
 
         if (type(value) in [list, int, float, str, bool] or isinstance(value, date) or value is None) and type(validation) == str:
@@ -106,13 +120,13 @@ class YMLValidator:
             try:
                 validator.validate()
             except ValueError as e:
-                raise ValueError(f"YML validation error at path '{path}': {str(e)}")
+                self.field_errors.append(f"YML validation error at path '{path}': {str(e)}")
             return
 
-        raise ValueError(f"YML parsing error: value='{value}', validation='{validation}'")
+        self.field_errors.append(f"YML parsing error: value='{value}', validation='{validation}'")
 
 
 if __name__ == "__main__":
-    v = YMLValidator("eth-ethereum")
+    v = YMLValidator("bat-basic-attention-token")
     v.validate()
 
