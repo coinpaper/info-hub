033f2e37bde81be1aff132408e8c04a210bb4a33
[server] joelbarmettlerUZH <admin@coinpaper.io>
2020-10-10 17:29:32 +0000

    changed print in validate.py

--- validate.py
+++ validate.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     coins = get_coins()
     print(coins)
     for coin in coins:
-        print(f"***{coin}***")
+        print(f"*** {coin} ***")
         validator = YMLValidator(coin)
         validator.validate()
 
